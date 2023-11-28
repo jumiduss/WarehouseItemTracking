66fcac9384cf617e5ba10bcfe802a5a0e3943385
jumiduss <jumiduss@gmail.com>
2023-11-28 07:16:00 -0500

    Project has been tabled after a series of setbacks this past week. Starting a udemy backend webdevelopment course to gain the background that self-directed research couldn't provide.

--- WarehouseImage/SVG Layout/shelfObjects/shelfCreation.js
+++ WarehouseImage/SVG Layout/shelfObjects/shelfCreation.js
@@ -1,57 +1,52 @@
-const newRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+// Array Format ["nShelf", "nSpace", "wTotal", "hTotal", "wSupport", "wCenter", "hSpace", "hShelf"]
+const shelfArray = fetch('shelfCoord.json'[shelfClass]);
 
-class Shelf {
+nShelf = shelfArray[0];
+nSpace = shelfArray[1];
+wTotal = shelfArray[2];
+hTotal = shelfArray[3];
+wSupport = shelfArray[4];
+wCenter = shelfArray[5];
+hSpace = shelfArray[6];
+hShelf = shelfArray[7];
 
-    constructor(vaShelf) {
-        this.qSh = vaShelf[0];
-        this.qSp = vaShelf[1];
-        this.wT  = vaShelf[2];
-        this.wSh = vaShelf[3];
-        this.wSu = vaShelf[4];
-        this.hU  = vaShelf[5];
-        this.hSh = vaShelf[6];
-        this.hB  = vaShelf[7];
-        this.hSp = vaShelf[8];
-        this.hT  = vaShelf[9];
-    }
-    // build() {
-    //     gsap.set(newRect, {
-    //         class:shelf,
-    //         x: this.wSu,
-    //         y: (this.hSp * (i + 1)) + (i * this.hSh),
+const viewPort = (wTotal,hTotal);
+var xStart = 0;
+var yStart = 0;
 
-    //     });
-    // }
+// Side Supports
+const hSupport = hTotal - 
+createRectangle(xStart, hSpace, wSupport, hTotal - hSpace);
+createRectangle(wSupport + wCenter, hSpace, wSupport, hTotal-hSpace);
+// Top Space
+yStart += createRectangle(xStart, yStart, wTotal, hSpace, nShelf);
+xStart += wSupport;
+// Top Shelf
+yStart += createRectangle(wSupport, xStart, wCenter, hShelf);
+
+// Only Applies to Pallet Racks which have nSpace = 3, nShelf = 2, group is always 1 and Shelf Class is Split (true)
+if (nShelf !== nSpace) {
+    // Space Above Shelf 1
+    yStart += createRectangle(xStart,yStart,wCenter,hSpace,1,true);
+    // Shelf 1
+    yStart += createRectangle(xStart,yStart,wCenter,hShelf,1,true);
+    // Space Below Shelf 1
+    createRectangle(xStart,yStart,wCenter,hSpace,1,true);
+}
+else {
+    for (i == 2; i <= arrShelf[1]; i ++) {
+        var numOfGroup = nSpace + 1 - i;
+        //Space
+        yStart += createRectangle(xStart,yStart,wCenter,hSpace,numOfGroup);
+        //Shelf
+        yStart += createRectangle(xStart,yStart,wCenter,hShelf,numOfGroup);
+    }    
 }
 
-class StructuretoMake {
-    build() {
-        
-        open()
+
+function createRectangle(startX, startY, width, height, groupNum, splitShelf = false) {
 
 
-
-
-
-
-
-    }
+    return height;
 }
-
-
-const pShelf = {
-
-       //   qSh, qSp,  wT, wSh, wSu,  hU, hSh,  hB,    hSp,     hT 
-    "r4"  :[  2,   3,  54,   3,  48,  84,   4,   0,     38,    122], //Rack 4'
-    "r8"  :[  2,   3, 102,   3,  96,  84,   4,   0,     38,    122], //Rack 8'
-    "s464":[  4,   4,   2,  44,  48,  72,   2, 2.5,   22.5,  100.5], //Shelf 4x6x4
-    "s465":[  5,   5,   2,  44,  48,  72,   2,   2,     17,     97], //Shelf 4x6x5
-    "s474":[  4,   4,   3,  42,  48,  84,   3,   0,     27,    120], //Shelf 4x7x4
-    "s475":[  5,   5,   3,  42,  48,  84,   3,   0, 200.25, 116.25], //Shelf 4x7x5
-    "s564":[  4,   4,   2,  56,  60,  72,   2, 2.5,   22.5,  100.5], //Shelf 5x6x4
-    "s565":[  5,   5,   2,  56,  60,  72,   2,   2,     17,     97], //Shelf 5x6x5
-    "s574":[  4,   4,   3,  54,  60,  84,   3,   0,     27,    120], //Shelf 5x7x4
-    "s575":[  5,   5,   3,  54,  60,  84,   3,   0,  20.25, 116.25]  //Shelf 5x7x5
-
-};
 
